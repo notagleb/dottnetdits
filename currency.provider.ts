f19c89a9418fa74de71acecd2adfe779339fd503
notagleb <illia.hliebov@gmail.com>
2020-05-06 01:59:15 +0300

    4.2 Added prototype of cached currency class. It caches any data it reads. It returns valid data from cache by default

--- currency.provider.ts
+++ currency.provider.ts
@@ -1,15 +1,34 @@
+import { TimeProvider } from "./time.provider";
+
 export abstract class CurrencyProvider {
     abstract getCurrency(currencyCode: string): Currency;
 }
+export abstract class Currency {
+    public abstract get code(): string;
+    // public getExchangeRateFor(currencyCode: string): number {
+    //     const rates = this.context
+    //         .exchangeRates
+    //         .filter((r: any) => r.currencyCode === currencyCode || this.code)
 
-export class Currency {
-    constructor() {}
-    public code: string = '';
+    //     return rates[currencyCode].rate/rates[this.code].rate;
+    // };
+    public abstract getExchangeRateFor(currencyCode: string): number;
+}
+
+export class CachingCurrency implements Currency {
+    private readonly cache: Object;
     public getExchangeRateFor(currencyCode: string): number {
-        const rates = this.context
-            .exchangeRates
-            .filter((r: any) => r.currencyCode === currencyCode || this.code)
+        const cacheEntry:{ [key: string]: any} = {};
+        if (
+            this.cache.tryGetValue(currencyCode, cacheEntry) &&
+            !cacheEntry.isExpired
+        ) {
+            return cacheEntry.exchangeRate;
+        }
+        const exchangeRate = this.innerCurrency.getExchangeRateFor(currencyCode);
+        const expiration = TimeProvider.Current.UtcNow + this.cacheTimeout;
+        this.cache[currencyCode] = new CurrencyCacheEntry(exchangeRate, expiration);
 
-        return rates[currencyCode].rate/rates[this.code].rate;
-    };
+        return exchangeRate;
+    }
 }
